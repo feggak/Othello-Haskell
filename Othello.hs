1c5c42b2f79bf81dec3367fd69a38e889435ee5c
Fredrik Kindstrom <fredrik.kindstrom@gmail.com>
2016-12-10 21:49:00 +0100

    cleanup again

--- Othello.hs
+++ Othello.hs
@@ -1,4 +1,5 @@
 module Othello where
+
 import Data.Maybe
 
 type Pos = (Int, Int)
@@ -39,7 +40,7 @@ list !!= (i,e) | i >= length list || (i < 0) = list
                 | otherwise = take i list ++ [e] ++ drop (i+1) list
 
 play :: Board -> Maybe Disk -> Pos -> Board
-play b d p | isCandidate b d p = flipDisks (placeDisk b d p) d (nextTo b d p)
+play b d p | isCandidate b d p = flipDisks (placeDisk b d p) d (cellsToFlip b d p)
            | otherwise = b
 
 isLegal :: Board -> Pos -> Bool
@@ -50,13 +51,15 @@ flipDisks b d (x:[]) = placeDisk b d x
 flipDisks b d (x:xs) = flipDisks (placeDisk b d x) d xs
 
 -- || returns a list of positions of disks to flip
-getCellsToFlip :: Board -> Maybe Disk -> Pos -> [Pos] -> [Pos]
-getCellsToFlip b d (x,y) [] = []
-getCellsToFlip b d (x,y) (z:[]) = getRow b d z (fst z - x, snd z - y)
-getCellsToFlip b d (x,y) (z:zs) = getRow b d z (fst z - x, snd z - y) ++ getCellsToFlip b d (x,y) zs
---where
---  a = x - fst z
---  b = y - snd z
+cellsToFlip :: Board -> Maybe Disk -> Pos -> [Pos]
+cellsToFlip b d (x,y) = cellsToFlip' b d (x,y) (filterNeighbours b list d)
+                     where
+                       list = neighbours b [(i,j) | i <- [x+1, x, x-1], j <- [y+1, y, y-1]]
+
+cellsToFlip' :: Board -> Maybe Disk -> Pos -> [Pos] -> [Pos]
+cellsToFlip' b d (x,y) [] = []
+cellsToFlip' b d (x,y) (z:[]) = getRow b d z (fst z - x, snd z - y)
+cellsToFlip' b d (x,y) (z:zs) = getRow b d z (fst z - x, snd z - y) ++ cellsToFlip' b d (x,y) zs
 
 getRow :: Board -> Maybe Disk -> Pos -> Pos -> [Pos]
 getRow b d (x,y) (e,f) | d == getDisk b (x,y) = []
@@ -65,22 +68,17 @@ getRow b d (x,y) (e,f) | d == getDisk b (x,y) = []
 getDisk :: Board -> Pos -> Maybe Disk
 getDisk b (x,y) = (b!!x)!!y
 
-nextTo :: Board -> Maybe Disk -> Pos -> [Pos]
-nextTo b d (x,y) = getCellsToFlip b d (x,y) (nextTo' b list d)
-                     where
-                       list = neighbours b [(i,j) | i <- [x+1, x, x-1], j <- [y+1, y, y-1]]
-
 neighbours :: Board -> [Pos] -> [Pos]
-neighbours b ((x,y):[]) | x <= 0 || y <= 0 || x >= length b -1 || y >= length b -1 = []
+neighbours b ((x,y):[]) | x <= 0 || y <= 0 || x > length b || y > length b = []
                         | otherwise = [(x,y)]
-neighbours b ((x,y):xs) | x <= 0 || y <= 0 || x >= length b -1 || y >= length b -1 = [] ++ neighbours b xs
+neighbours b ((x,y):xs) | x <= 0 || y <= 0 || x > length b || y > length b = [] ++ neighbours b xs
                         | otherwise = [(x,y)] ++ neighbours b xs
 
-nextTo' :: Board -> [Pos] -> Maybe Disk -> [Pos]
-nextTo' b (x:[]) d | getDisk b x == d || isNothing(getDisk b x) = []
+filterNeighbours :: Board -> [Pos] -> Maybe Disk -> [Pos]
+filterNeighbours b (x:[]) d | getDisk b x == d || isNothing(getDisk b x) = []
                    | otherwise = [x]
-nextTo' b (x:xs) d | getDisk b x == d || isNothing(getDisk b x) = [] ++ nextTo' b xs d
-                   | otherwise = [x] ++ nextTo' b xs d
+filterNeighbours b (x:xs) d | getDisk b x == d || isNothing(getDisk b x) = [] ++ filterNeighbours b xs d
+                   | otherwise = [x] ++ filterNeighbours b xs d
 
 isCandidate :: Board -> Maybe Disk -> Pos -> Bool
-isCandidate b d (x,y) = isLegal b (x,y) && isNothing ((b!!x)!!y) && (nextTo b d (x,y) /= [])
+isCandidate b d (x,y) = isLegal b (x,y) && isNothing ((b!!x)!!y) && (cellsToFlip b d (x,y) /= [])
