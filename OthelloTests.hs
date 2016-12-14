da8e495dbbfd274f4fcb247e740b9bf5432bdd4c
Fredrik Kindstrom <fredrik.kindstrom@gmail.com>
2016-12-14 18:14:04 +0100

    added first test stub

--- OthelloTests.hs
+++ OthelloTests.hs
@@ -2,3 +2,6 @@ module OthelloTests where
 
 import Othello
 import Test.QuickCheck
+
+prop_flipDisk :: Board -> Maybe Disk -> [Pos] -> Bool
+prop_flipDisk = undefined
--- OthelloUtils.hs
+++ OthelloUtils.hs
@@ -1,6 +1,7 @@
 module OthelloUtils where
 
 -- | updates the given list with the new value at the given index
+-- | from the Sudoku lab
 (!!=) :: [a] -> (Int, a) -> [a]
 list !!= (i,e) | i >= length list || (i < 0) = list
                | otherwise = take i list ++ [e] ++ drop (i+1) list
